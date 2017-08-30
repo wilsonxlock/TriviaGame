22fc2d0e491502abd3465fd08e07b1a85d26876b
Tyler Wilson <x@Tylers-MacBook-Pro.local>
2017-08-30 18:28:58 -0400

    start page and page switching logic working

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,28 +13,59 @@ var questions = [
     }
     }
 ]
+function start(){
+    $(".choice1").html("Start")
+    $(".choice2").hide()
+    $(".choice3").hide()
+    $(".choice4").hide()
+    $(".choice1").on("click", function(){
+        qpage1()
+    })
+}
 function qpage1 (){$(".h1").html(questions[0].string1);
+$(".timer").hide()
+$(".buttons").show()
+$(".choice2").show()
+$(".choice3").show()
+$(".choice4").show()
 $(".choice1").html(questions[0].choice.right)
 $(".choice2").html(questions[0].choice.wrong[0])
 $(".choice3").html(questions[0].choice.wrong[1])
 $(".choice4").html(questions[0].choice.wrong[2])
 $(".choice2,.choice3,.choice4").on("click",function(){
-    console.log("wrong")
-    wrongscreen("choice1",'<img src="https://media.giphy.com/media/3ohzdYTdnKMNqY2eJO/giphy.gif">')
+    $(".h1").html("Wrong the correct answer is ");
+    $(".timer").show()
+    $(".timer").html()
+    $(".buttons").hide()
+    setTimeout(function(){ qpage2(); }, 3000);
 })
 $(".choice1").on("click",function(){
-    console.log("right")
-    rightscreen()
+    $(".h1").html("Correct!")
+    $(".buttons").html()}
 
+
+
+)}
+function qpage2 (){$(".h1").html(questions[1].string1);
+$(".timer").hide()
+$(".buttons").show()
+$(".choice1").html(questions[0].choice.right)
+$(".choice2").html(questions[0].choice.wrong[0])
+$(".choice3").html(questions[0].choice.wrong[1])
+$(".choice4").html(questions[0].choice.wrong[2])
+$(".choice2,.choice3,.choice4").on("click",function(){
+    $(".h1").html("Wrong the correct answer is ");
+    $(".timer").show()
+    $(".timer").html()
+    $(".buttons").hide()
+    setTimeout(function(){ qpage2(); }, 3000);
 })
-}
-function wrongscreen (x,y){
-    $(".h1").html("Wrong the correct answer is "+ x);
-    $(".buttons").html(y)
-    
-}
-function rightscreen (y){
-$(".h1").html("Correct!")
-$(".buttons").html(y)}
-qpage1()
+$(".choice1").on("click",function(){
+    $(".h1").html("Correct!")
+    $(".buttons").html()}
+
+
+
+)}
+start()
 })
