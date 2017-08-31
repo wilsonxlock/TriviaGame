5f21ea8a12a312b2f8fb99238b0148e1fd7b92af
Tyler Wilson <x@tylers-mbp.attlocal.net>
2017-08-30 22:08:53 -0400

    still working out the timers, unable to timeout per question

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,18 +1,8 @@
 $( document ).ready(function() {
-var questions = [
-    {string1: "question1",
-    choice: {
-        right: "answer 1",
-        wrong: ["answer2", "answer3","answer4"]
-    }
-    },
-    {string2: "question2",
-    choice: {
-        right: "answer 1",
-        wrong: ["answer2", "answer3","answer4"]
-    }
-    }
-]
+var time = "60";
+function timeup (){time--;$(".timer").html("Time left " + time);if (time == "0"){
+     $(".timer").html('<img src="assets/javascript/images/donttread.JPG">');}};
+function timer(){setInterval(function(){timeup()},1000);}
 function start(){
     $(".choice1").html("Start")
     $(".choice2").hide()
@@ -22,20 +12,20 @@ function start(){
         qpage1()
     })
 }
-function qpage1 (){$(".h1").html(questions[0].string1);
-$(".timer").hide()
+function qpage1 (){
+$(".h1").html("whats my favorite color?");
+$(".timer").html("time left " + time)
+timer()
 $(".buttons").show()
 $(".choice2").show()
 $(".choice3").show()
 $(".choice4").show()
-$(".choice1").html(questions[0].choice.right)
-$(".choice2").html(questions[0].choice.wrong[0])
-$(".choice3").html(questions[0].choice.wrong[1])
-$(".choice4").html(questions[0].choice.wrong[2])
+$(".choice1").html("blue")
+$(".choice2").html("green")
+$(".choice3").html("pink")
+$(".choice4").html("red")
 $(".choice2,.choice3,.choice4").on("click",function(){
     $(".h1").html("Wrong the correct answer is ");
-    $(".timer").show()
-    $(".timer").html()
     $(".buttons").hide()
     setTimeout(function(){ qpage2(); }, 3000);
 })
@@ -46,26 +36,51 @@ $(".choice1").on("click",function(){
 
 
 )}
-function qpage2 (){$(".h1").html(questions[1].string1);
-$(".timer").hide()
+function qpage2 (){
+$(".h1").html("where was i born");
+$(".gif").hide()
+$(".timer").html("time left " + time)
 $(".buttons").show()
-$(".choice1").html(questions[0].choice.right)
-$(".choice2").html(questions[0].choice.wrong[0])
-$(".choice3").html(questions[0].choice.wrong[1])
-$(".choice4").html(questions[0].choice.wrong[2])
+$(".choice1").html("winter haven")
+$(".choice2").html("lakeland")
+$(".choice3").html("tampa")
+$(".choice4").html("florida")
 $(".choice2,.choice3,.choice4").on("click",function(){
     $(".h1").html("Wrong the correct answer is ");
-    $(".timer").show()
-    $(".timer").html()
+    $(".gif").show()
+    $(".gif").html('<img src="assets/javascript/images/donttread.JPG">')
     $(".buttons").hide()
-    setTimeout(function(){ qpage2(); }, 3000);
+    setTimeout(function(){ qpage3(); }, 3000);
 })
 $(".choice1").on("click",function(){
     $(".h1").html("Correct!")
-    $(".buttons").html()}
-
-
+    $(".gif").show()
+    $(".gif").html('<img src="assets/javascript/images/donttread.JPG">')
+    $(".buttons").hide()
+    setTimeout(function(){ qpage3(); }, 3000);
+}
 
 )}
+function qpage3 (){
+    $(".gif").hide()
+    $(".h1").html("do i know what im doing");
+    $(".timer").html("time left " + time)
+    $(".buttons").show()
+    $(".choice1").html("no")
+    $(".choice2").html("no")
+    $(".choice3").html("no")
+    $(".choice4").html("no")
+    $(".choice2,.choice3,.choice4").on("click",function(){
+        $(".h1").html("Wrong the correct answer is ");
+        $(".buttons").hide()
+        setTimeout(function(){ qpage3(); }, 3000);
+    })
+    $(".choice1").on("click",function(){
+        $(".h1").html("Correct!")
+        $(".buttons").html()}
+    
+    
+    
+    )}
 start()
 })
